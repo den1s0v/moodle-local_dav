09a24e838635fdb651755264dbcd2f48612e771f
den1s0v <hello-lumen@ya.ru>
2025-08-25 04:05:42 +0300

    Add missing lang strings

---
+++ dav/lang/en/local_dav.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// (c) Your Name / Your Org.
+
+$string['pluginname'] = 'Local DAV (WebDAV integration)';
+$string['picodavini'] = '.picodav.ini contents';
+$string['picodavini_desc'] = 'Raw text to be written to moodledata/local_dav/.picodav.ini. You can paste entire PicoDAV configuration here. Example:
+ANONYMOUS_READ = true
+ANONYMOUS_WRITE = false
+# ROOT = storage';
+$string['wherefiles_title'] = 'Paths & endpoint';
+$string['wherefiles_desc']  = 'The WebDAV endpoint will be available at: <code>{$a->web}</code><br>
+The <code>.picodav.ini</code> will be stored under <code>moodledata/local_dav/.picodav.ini</code>.<br>
+Files by default go to <code>moodledata/local_dav/storage</code> unless you set <code>ROOT</code> in the ini.';
